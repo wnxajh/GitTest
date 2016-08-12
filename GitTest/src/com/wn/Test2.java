af51ba871ad62d0aef6e5599eac46c9eba2184f4
wunan <wunan@localhost>
2016-08-12 14:35:42 +0800

    sdd

--- GitTest/src/com/wn/Test2.java
+++ GitTest/src/com/wn/Test2.java
@@ -3,5 +3,9 @@ package com.wn;
 public class Test2 {
          public static void main(String[] args) {
 			System.out.println("292200");
+			
+			int as =11;
+			
+			System.out.println(as);
 		}
 }
